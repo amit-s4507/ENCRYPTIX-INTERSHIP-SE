1494d4d794a93201356d9d2a413ee28e5a09d8f2
Amit Sahu <amitsahu174507@gmail.com>
2024-05-28 23:34:19 +0530

    Generating the random numbers

--- guessing_no.cpp
+++ guessing_no.cpp
@@ -1,11 +1,36 @@
 // Guessing The Number Game 
 
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
+
+// Random Number : Choosen by Computer
 int main(){
 
-    cout<<"        Welcome To the Guessing Game!"<<endl;
+   
+    
+    // for generating random numbers by computer
+
+
+    // now for generating between limits 0 to 100
+
+    // int random = offset + (rand() % range)
+    // int random = 1 + (rand()% 100)
+
+
+    srand(time(0));
+    int random = 1 + (rand()% 100);
+
+   
+    cout<<"\n\t\t\t Welcome To the Guessing Game!"<<endl;
+    cout<<"\n You have to Guess a Number between 1 and 100 . You'll have limited choices based on the Difficulty Level you choose"<<endl;
+    cout<<" Good luck !!! "<<endl;
+
+
+
+
 
  return 0;
 
